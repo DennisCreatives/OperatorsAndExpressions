f68303762957d9b8e7152b8f0e01cedcf8335d02
DennisCreatives <creatives.dennis@gmail.com>
2024-12-09 14:18:22 +0300

    First Commit

---
+++ src/OperatorsAndExpression.java
@@ -0,0 +1,34 @@
+public class OperatorsAndExpression {
+    // Operators and Expressions
+
+    public static void main (String[] args) {
+        // 1. Arithmetic
+        // Java provides several arithmetic operators to perform mathematical operations on numerical values.
+        // These operators are:
+        int x = 10;
+        int y = 3;
+
+        // 1. Addition (+) - adds two values
+        int sum = x + y;
+        System.out.println("The sum is: " + sum);
+
+        // 2. Subtraction (-) - subtracts the second value from the first value
+        int difference = x - y;
+        System.out.println("The difference is: " + difference);
+
+        // 3. Multiplication (*) - multiplies two values
+        int product = x * y;
+        System.out.println("The product is: " + product);
+
+        // 4. Division (/) - Divides the first value by the second value
+        double quotient = (double) x / (double) y;
+        System.out.println("The quotient is : " + quotient);
+
+        // 5. Modulus (%) - Returns the remainder after dividing the first value by second value
+        int modulus = x % y;
+        System.out.println("The modulo is: " + modulus);
+
+        // B. Relational Operators
+        //
+    }
+}
